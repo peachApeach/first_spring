ed7508a52cd8c630716c965291cd1c18b22d08f9
peachApeach <yh_01@daum.net>
2025-02-20 23:45:03 +0900

    ADD: add funtion of join member

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String creatForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        // 회원 가입이 끝나면 홈 화면으로 돌아가도록 설정
+        return "redirect:/";
     }
 }
